75dc1d054d9d7ca6342ceb65c1ed12f2e1173b53
Florian Ziller <existuus@gmail.com>
2025-01-30 09:06:04 +0100

    [10m] Add some tests.

--- client/src/hooks/__tests__/useModelFilter.spec.tsx
+++ client/src/hooks/__tests__/useModelFilter.spec.tsx
@@ -1,30 +1,80 @@
-import { act, renderHook } from "@testing-library/react-hooks";
-import { useModelFilter } from "../useModelFilter";
+import { act, renderHook } from "@testing-library/react";
+import { ModelActionType, useModelFilter } from "../useModelFilter";
 
 describe("useModelFilter", () => {
+  const defaultState = {
+    event: [],
+    gender: [],
+    genre: [],
+    height: undefined,
+    width: undefined,
+  };
   it("initializes with default state", () => {
     const { result } = renderHook(() => useModelFilter());
-    expect(result.current.state).toEqual({
-      // default state values
-    });
+    expect(result.current.state).toEqual(defaultState);
   });
 
-  it("updates state when dispatch is called", () => {
+  it("updates gender state when dispatch is called", () => {
     const { result } = renderHook(() => useModelFilter());
     act(() => {
-      result.current.dispatch({ type: "SET_GENDER", payload: "male" });
+      result.current.dispatch({
+        type: ModelActionType.SET_GENDER,
+        payload: "male",
+      });
     });
     expect(result.current.state.gender).toBe("male");
+  });
+
+  it("updates width state when dispatch is called", () => {
+    const { result } = renderHook(() => useModelFilter());
+    act(() => {
+      result.current.dispatch({ type: ModelActionType.SET_WIDTH, payload: 12 });
+    });
+    expect(result.current.state.width).toBe(12);
+  });
+
+  it("updates height state when dispatch is called", () => {
+    const { result } = renderHook(() => useModelFilter());
+    act(() => {
+      result.current.dispatch({
+        type: ModelActionType.SET_HEIGHT,
+        payload: 24,
+      });
+    });
+    expect(result.current.state.height).toBe(24);
+  });
+
+  it("updates genre state when dispatch is called", () => {
+    const { result } = renderHook(() => useModelFilter());
+    act(() => {
+      result.current.dispatch({
+        type: ModelActionType.SET_GENRE,
+        payload: ["action", "fantasy"],
+      });
+    });
+    expect(result.current.state.genre).toStrictEqual(["action", "fantasy"]);
+  });
+
+  it("updates event state when dispatch is called", () => {
+    const { result } = renderHook(() => useModelFilter());
+    act(() => {
+      result.current.dispatch({
+        type: ModelActionType.SET_EVENT,
+        payload: ["event1", "event2"],
+      });
+    });
+    expect(result.current.state.event).toStrictEqual(["event1", "event2"]);
   });
 
   it("resets state when reset action is dispatched", () => {
     const { result } = renderHook(() => useModelFilter());
     act(() => {
-      result.current.dispatch({ type: "SET_GENDER", payload: "male" });
-      result.current.dispatch({ type: "RESET" });
+      result.current.dispatch({
+        type: ModelActionType.SET_GENDER,
+        payload: "male",
+      });
+      result.current.dispatch({ type: ModelActionType.RESET, payload: [] });
     });
-    expect(result.current.state).toEqual({
-      // default state values
-    });
+    expect(result.current.state).toEqual(defaultState);
   });
 });
--- client/src/hooks/__tests__/useShirtFilter.spec.tsx
+++ client/src/hooks/__tests__/useShirtFilter.spec.tsx
@@ -1,5 +1,5 @@
-import { act, renderHook } from "@testing-library/react-hooks";
-import { useShirtFilter } from "../useShirtFilter";
+import { act, renderHook } from "@testing-library/react";
+import { ShirtActionType, useShirtFilter } from "../useShirtFilter";
 
 describe("useShirtFilter", () => {
   it("initializes with default state", () => {
@@ -11,19 +11,47 @@ describe("useShirtFilter", () => {
     });
   });
 
-  it("updates state when dispatch is called", () => {
+  it("updates size state when dispatch is called", () => {
     const { result } = renderHook(() => useShirtFilter());
     act(() => {
-      result.current.dispatch({ type: "SET_SIZE", payload: ["small"] });
+      result.current.dispatch({
+        type: ShirtActionType.SET_SIZE,
+        payload: ["small"],
+      });
     });
     expect(result.current.state.size).toEqual(["small"]);
+  });
+
+  it("updates color state when dispatch is called", () => {
+    const { result } = renderHook(() => useShirtFilter());
+    act(() => {
+      result.current.dispatch({
+        type: ShirtActionType.SET_COLOR,
+        payload: ["red", "blue"],
+      });
+    });
+    expect(result.current.state.color).toEqual(["red", "blue"]);
+  });
+
+  it("updates brand state when dispatch is called", () => {
+    const { result } = renderHook(() => useShirtFilter());
+    act(() => {
+      result.current.dispatch({
+        type: ShirtActionType.SET_BRAND,
+        payload: "BERND BRAND",
+      });
+    });
+    expect(result.current.state.brand).toEqual("BERND BRAND");
   });
 
   it("resets state when reset action is dispatched", () => {
     const { result } = renderHook(() => useShirtFilter());
     act(() => {
-      result.current.dispatch({ type: "SET_SIZE", payload: ["small"] });
-      result.current.dispatch({ type: "RESET" });
+      result.current.dispatch({
+        type: ShirtActionType.SET_SIZE,
+        payload: ["small"],
+      });
+      result.current.dispatch({ type: ShirtActionType.RESET, payload: [] });
     });
     expect(result.current.state).toEqual({
       size: [],
